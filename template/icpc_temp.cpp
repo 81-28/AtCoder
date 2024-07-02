d5f9b5d7d83a14d6f2fdcb4096392cb372b2d851
81-28 <eskrmc@gmail.com>
2024-07-02 16:57:13 +0900

    temp_update

--- template/icpc_temp.cpp
+++ template/icpc_temp.cpp
@@ -8,12 +8,38 @@
 using namespace std;
 // #include <atcoder/all>
 // using namespace atcoder;
+
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define rep1(i, n) for (int i = 1; i < (int)(n+1); i++)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+#define substring(s,l,r) s.substr(l,r-l)
 using ll = long long;
-// using P = pair<int,int>;
-// using Graph = vector<vector<int>>;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
 // using mint = modint1000000007;
+// using mint = modint998244353;
+
+vector<int> input(int n) {
+    vector<int> As;
+    As.reserve(n);
+    rep(i,n) {
+        int A;
+        cin >> A;
+        As.push_back(A);
+    }
+    return As;
+}
+void print(vector<int> anss) {
+    rep(i,anss.size()) cout << anss[i] << " ";
+    cout << endl;
+}
+void printn(vector<int> anss) {
+    rep(i,anss.size()) cout << anss[i] << endl;
+}
+
+
 
 int main() {
     vector<int> anss;
@@ -26,15 +52,23 @@ int main() {
         {
             break;
         } else {
-            int ans = n;
+            int ans = 0;
+            vector<int> as;
+            rep(i,n) {
+                int a;
+                cin >> a;
+                as.push_back(a);
+            }
+
+
 
             anss.push_back(ans);
             anss[index] += ans;
             index++;
         }
     }
-    
-    rep(i,anss.size()) cout << anss[i] << endl;
+
+    printn(anss);
 
     return 0;
 }
--- template/temp.cpp
+++ template/temp.cpp
@@ -8,31 +8,56 @@
 using namespace std;
 // #include <atcoder/all>
 // using namespace atcoder;
+
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define rep1(i, n) for (int i = 1; i < (int)(n+1); i++)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+#define substring(s,l,r) s.substr(l,r-l)
 using ll = long long;
 using P = pair<int,int>;
 using Graph = vector<vector<int>>;
 // using mint = modint1000000007;
+// using mint = modint998244353;
+
+vector<int> input(int n) {
+    vector<int> As;
+    As.reserve(n);
+    rep(i,n) {
+        int A;
+        cin >> A;
+        As.push_back(A);
+    }
+    return As;
+}
+void print(vector<int> anss) {
+    rep(i,anss.size()) cout << anss[i] << " ";
+    cout << endl;
+}
+void printn(vector<int> anss) {
+    rep(i,anss.size()) cout << anss[i] << endl;
+}
+
+
 
 int main() {
+    int ans = 0;
+    vector<int> anss;
+
     int n;
     cin >> n;
-    cout << n << endl;
+    vector<int> as = input(n);
 
 
-    string strN;
-    // 数字を入力
-    cin >> strN;
-    string newStr = strN;
-    rep(i, strN.length()){
-        // 文字を数字として扱う
-        int num = (strN[i] - '0');
-        cout << num << endl;
-        // 前後を反転
-        newStr[i] = strN[strN.length()-1-i];
-    }
-    cout << newStr << endl;
+
+    anss = as;
+
+
+
+    cout << ans << endl;
+    print(anss);
+    printn(anss);
 
 
     return 0;
