100571efd0f926b4dbb9e491bdb4a25335b46c06
81-28 <eskrmc@gmail.com>
2024-10-18 22:01:36 +0900

    isP

--- template/functions.cpp
+++ template/functions.cpp
@@ -114,20 +114,10 @@ ll lcm(const ll& a, const ll& b) {
 
 // 素数判定する関数
 bool isPrime(const int& n) {
-    if (n <= 1) {
-        return false;
-    } else if (n <= 3) {
-        return true;
-    } else if (n % 2 == 0 || n % 3 == 0) {
-        return false;
-    }
-    int i = 5;
-    while (i * i <= n) {
-        if (n % i == 0 || n % (i + 2) == 0) {
-            return false;
-        }
-        i += 6;
-    }
+    if (n <= 1) return false;
+    else if (n <= 3) return true;
+    else if (!(n%2&&n%3)) return false;
+    for (int i=5;i*i<=n;i+=6) if (!(n%i&&n%(i+2))) return false;
     return true;
 }
 // 素因数分解する関数
