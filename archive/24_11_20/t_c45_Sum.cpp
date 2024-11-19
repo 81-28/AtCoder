5078ea64f5d7629c5fb474523cc4ce0d02a9b136
81-28 <eskrmc@gmail.com>
2024-11-20 01:48:15 +0900

    24_11_20

---
+++ archive/24_11_20/t_c45_Sum.cpp
@@ -0,0 +1,11 @@
+// https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_dr
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+    long long a,b,c;
+    cin>>a>>b>>c;
+    cout<<(a+b+c==0?"Yes\n":"No\n");
+    return 0;
+}
