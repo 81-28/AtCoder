8e67493ace6a85e70f3e1b09715f68888d13ea85
81-28 <eskrmc@gmail.com>
2024-12-23 22:08:00 +0900

    24_12_23

---
+++ archive/24_12_23/abc336_a.cpp
@@ -0,0 +1,17 @@
+// https://atcoder.jp/contests/abc336/tasks/abc336_a
+
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0;i<(int)(n);i++)
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+
+    int n;
+    cin >> n;
+    cout<<'L';
+    rep(i,n) cout<<'o';
+    cout<<"ng\n";
+
+    return 0;
+}
