8683e63a959f3018c06f24bdc5ba1cac4655f996
81-28 <eskrmc@gmail.com>
2025-01-03 00:16:37 +0900

    25_01_03

---
+++ archive/25_01_03/abc333_a.cpp
@@ -0,0 +1,17 @@
+// https://atcoder.jp/contests/abc333/tasks/abc333_a
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define rep(i,n) for(int i=0;i<(int)(n);i++)
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+
+    int n;
+    cin >> n;
+    rep(i,n) cout<<n;
+    cout<<'\n';
+
+    return 0;
+}
