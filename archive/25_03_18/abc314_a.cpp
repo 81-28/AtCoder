0846080dfa6da646e4a0da7904e926709f605f36
81-28 <eskrmc@gmail.com>
2025-03-18 21:44:38 +0900

    25_03_18

---
+++ archive/25_03_18/abc314_a.cpp
@@ -0,0 +1,10 @@
+// https://atcoder.jp/contests/abc314/tasks/abc314_a
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s="3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+    int n;
+    cin>>n;
+    cout<<s.substr(0,n+2)+'\n';
+}
