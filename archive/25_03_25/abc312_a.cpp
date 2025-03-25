4d23c488de72acabf684e4cd9a797993362ab3cc
81-28 <eskrmc@gmail.com>
2025-03-25 23:39:57 +0900

    25_03_25

---
+++ archive/25_03_25/abc312_a.cpp
@@ -0,0 +1,10 @@
+// https://atcoder.jp/contests/abc312/tasks/abc312_a
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin >> s;
+    puts(s=="ACE"||s=="BDF"||s=="CEG"||s=="DFA"||s=="EGB"||s=="FAC"||s=="GBD"?"Yes":"No");
+    return 0;
+}
