7ba60f3190f034fd1d1f260c020bafb2e4219a76
81-28 <eskrmc@gmail.com>
2025-05-23 23:07:19 +0900

    25_05_23

--- archive/25_05_22/abc346_d_comp.cpp
+++ archive/25_05_22/abc346_d_comp.cpp
@@ -18,6 +18,7 @@ void print(const Head &head,const Tail &... tail){cout<<head;((cout<<' '<<tail),
 template<typename T>
 T min(const v<T>& v){return *min_element(v.begin(),v.end());}
 
+const ll inf=1LL<<60;
 
 int main(){
     ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
@@ -29,16 +30,11 @@ int main(){
     cin >> c;
     // dp[i][j]:i文字目までに状態j,kでの最低コスト
     // j:連続文字があったかどうか,文字iがk(0:1)
-    v<vvll> dp(n,vvll(2,vll(2,INT_MAX)));
+    v<vvll> dp(n,vvll(2,vll(2,inf)));
     bool b=s[0]-'0';
-    dp[0][0][0]=c[0]*b;
-    dp[0][0][1]=c[0]*!b;
-    for (int i=1; i<n; ++i) {
-        b=s[i]-'0';
-        dp[i][0][0] = dp[i-1][0][1]+c[i]*b;
-        dp[i][0][1] = dp[i-1][0][0]+c[i]*!b;
-        dp[i][1][0] = min(dp[i-1][0][0],dp[i-1][1][1])+c[i]*b;
-        dp[i][1][1] = min(dp[i-1][0][1],dp[i-1][1][0])+c[i]*!b;
+    rep(i,2) dp[0][0][i]=c[0]*(b^i);
+    for(ll i=1,b=s[i]-'0';i<n;++i,b=s[i]-'0')rep(j,2)rep(k,2) {
+        dp[i][j][k] = min(j?dp[i-1][0][k]:inf,dp[i-1][j][!k])+c[i]*(b^k);
     }
     print(min(dp[n-1][1]));
 
---
+++ archive/25_05_23/abc391_d_simple.cpp
@@ -0,0 +1,65 @@
+// https://atcoder.jp/contests/abc391/tasks/abc391_d
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define rep(i,n) for(int i=0;i<(int)(n);++i)
+#define pb push_back
+#define all(v) v.begin(),v.end()
+template<class T> constexpr bool chmax(T& a,T b){if(a<b){a=b;return 1;}return 0;}
+#define YesNo(x) puts(x?"Yes":"No")
+#define f first
+#define s second
+using ll=long long;
+template<typename T>
+using v=vector<T>;
+using vll=v<ll>;
+using pll=pair<ll,ll>;
+
+template<typename F,typename S>
+istream &operator>>(istream &is,pair<F,S> &p){is>>p.first>>p.second;return is;}
+
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+
+    ll n,w;
+    cin >> n >> w;
+    v<v<pll>> c(w);
+    pll p;
+    rep(i,n) {
+        cin >> p;
+        c[--p.f].pb({p.s,i});
+    }
+    for (auto vec:c) sort(all(vec));
+    bool ok=1;
+    // 層が消える暫定時間(デフォは無限)
+    vll ans(n+1,INT_MAX);
+    vll idx(n,n);
+    ll mx=0;
+    // i層目を見る
+    rep(i,n) {
+        rep(j,w) {
+            // i層のブロックが1つでも無かったら、i以上の層は消えない
+            if (i>=c[j].size()) {
+                ok=0;
+                break;
+            }
+            // c[j][i].s番のブロックが何層に属しているかを記録
+            idx[c[j][i].s]=i;
+            chmax(mx,c[j][i].f);
+        }
+        if (!ok) break;
+        // i層目が全部あったら、消える時間を記録
+        // i+1層目はi層が消えないと消えられない
+        ans[i]=mx++;
+    }
+    ll q;
+    cin >> q;
+    rep(i,q) {
+        cin >> p;
+        YesNo(p.f<ans[idx[--p.s]]);
+    }
+
+    return 0;
+}
