82895abac013fcaa25d9da3a216210d3d1a3cbe3
81-28 <eskrmc@gmail.com>
2025-05-26 14:47:14 +0900

    25_05_26

---
+++ archive/25_05_26/sumitb2019_a.cpp
@@ -0,0 +1,10 @@
+// https://atcoder.jp/contests/sumitrust2019/tasks/sumitb2019_a
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int m1,d,m2;
+    cin>>m1>>d>>m2>>d;
+    cout<<(m1!=m2)<<'\n';
+}
