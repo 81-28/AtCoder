c083e835f0e417e34b6b365bb862af66dacb64c7
81-28 <eskrmc@gmail.com>
2025-07-20 19:56:17 +0900

    25_07_20

---
+++ archive/25_07_20/abc298_d_mint.cpp
@@ -0,0 +1,45 @@
+// https://atcoder.jp/contests/abc298/tasks/abc298_d
+
+#include<bits/stdc++.h>
+using namespace std;
+#include<atcoder/all>
+using namespace atcoder;
+
+using mint=modint998244353;
+
+template<typename Head,typename... Tail>
+void print(const Head &head,const Tail &... tail){cout<<head;((cout<<' '<<tail),...);cout<<endl;}
+
+
+signed main(){
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+
+    int q;
+    cin >> q;
+    mint ans=1;
+    queue<mint> qu;
+    qu.push(1);
+    mint e=1;
+    while (q--) {
+        int n;
+        cin >> n;
+        if (n==1) {
+            int x;
+            cin >> x;
+            ans=ans*10+x;
+            qu.push(x);
+            e*=10;
+        }
+        if (n==2) {
+            mint m=qu.front();
+            qu.pop();
+            ans-=m*e;
+            e/=10;
+        }
+        if (n==3) {
+            print(ans.val());
+        }
+    }
+
+    return 0;
+}
