340c7b090eeeb3e643e581db079762e1400a48a4
81-28 <eskrmc@gmail.com>
2025-08-15 19:17:46 +0900

    25_08_15

---
+++ archive/25_08_15/abc293_a.cpp
@@ -0,0 +1,15 @@
+// https://atcoder.jp/contests/abc292/tasks/abc292_a
+
+#include<bits/stdc++.h>
+using namespace std;
+
+signed main(){
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+
+    string s;
+    cin >> s;
+    for (char c:s) cout<<(char)toupper(c);
+    cout<<endl;
+
+    return 0;
+}
