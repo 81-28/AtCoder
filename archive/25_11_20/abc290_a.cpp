a1ebb5176b9c607f12b28be7e4cd560960f7df03
81-28 <eskrmc@gmail.com>
2025-11-20 20:49:45 +0900

    25_11_20

---
+++ archive/25_11_20/abc290_a.cpp
@@ -0,0 +1,29 @@
+// https://atcoder.jp/contests/abc290/tasks/abc290_a
+
+#include<bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+using v=vector<T>;
+using vi=v<int>;
+#define rep(i,n) for(int i=0;i<(int)(n);++i)
+
+template<typename T>
+istream &operator>>(istream &is,v<T> &v){for(T &in:v)is>>in;return is;}
+template<typename Head,typename... Tail>
+void print(const Head &head,const Tail &... tail){cout<<head;((cout<<' '<<tail),...);cout<<endl;}
+
+
+signed main(){
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+
+    int n,m;
+    cin >> n >> m;
+    vi a(n),b(m);
+    cin >> a >> b;
+    int ans=0;
+    rep(i,m) ans+=a[b[i]-1];
+    print(ans);
+
+    return 0;
+}
--- template/unionfind.cpp
+++ template/unionfind.cpp
@@ -23,10 +23,11 @@ public:
     }
 
     // 頂点uとvを繋げる関数(sizが大きい方に根を統合)
-    void unite(const int& u, const int& v) {
+    // 返り値はsameと同じ
+    bool unite(const int& u, const int& v) {
         int rootU = root(u);
         int rootV = root(v);
-        if (rootU == rootV) return;
+        if (rootU == rootV) return 1;
         if (siz[rootU] < siz[rootV]) {
             par[rootU] = rootV;
             siz[rootV] += siz[rootU];
@@ -34,6 +35,7 @@ public:
             par[rootV] = rootU;
             siz[rootU] += siz[rootV];
         }
+        return 0;
     }
 
     // 頂点uとvが同じグループにあるか(同じ根を持つか)を返す関数
@@ -41,6 +43,7 @@ public:
         return root(u) == root(v);
     }
 
+    // 頂点xが含まれる連結成分の大きさ
     int size(const int& x) {
         return siz[root(x)];
     }
