4809617ddf405983e519bea9896d463126a25c8c
81-28 <eskrmc@gmail.com>
2025-11-30 00:40:44 +0900

    25_11_30

---
+++ archive/25_11_30/abc288_b.cpp
@@ -0,0 +1,28 @@
+// https://atcoder.jp/contests/abc288/tasks/abc288_b
+
+#include<bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+using v=vector<T>;
+#define rep(i,n) for(int i=0;i<(int)(n);++i)
+#define all(v) v.begin(),v.end()
+
+template<typename T>
+istream &operator>>(istream &is,v<T> &v){for(T &in:v)is>>in;return is;}
+template<typename Head,typename... Tail>
+void print(const Head &head,const Tail &... tail){cout<<head;((cout<<' '<<tail),...);cout<<endl;}
+
+
+signed main(){
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+
+    int n,k;
+    cin >> n >> k;
+    v<string> s(k);
+    cin >> s;
+    sort(all(s));
+    rep(i,k) print(s[i]);
+
+    return 0;
+}
