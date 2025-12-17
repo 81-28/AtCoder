3d84fdd46b4c24289cfa966deebd3f02f5ef609a
81-28 <eskrmc@gmail.com>
2025-12-17 16:13:55 +0900

    25_12_17

---
+++ archive/25_12_17/abc355_f_UnionFind_MST.cpp
@@ -0,0 +1,65 @@
+// https://atcoder.jp/contests/abc355/tasks/abc355_f
+
+#include<bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+using v=vector<T>;
+#define rep(i,n) for(int i=0;i<(int)(n);++i)
+
+template<typename Head,typename... Tail>
+void print(const Head &head,const Tail &... tail){cout<<head;((cout<<' '<<tail),...);cout<<endl;}
+
+
+class UnionFind {
+public:
+    // par: 親, siz: サイズ
+    vector<int> par,siz;
+    // n頂点のUnion-Find
+    UnionFind(const int& n) {
+        par = vector<int>(n,-1);
+        siz = vector<int>(n,1);
+    }
+
+    // 頂点xの根を返す関数
+    int root(const int& x) {
+        if (par[x] == -1) return x; // 親が無いとき、それが根
+        else return par[x] = root(par[x]); // 経路圧縮して親に移動
+    }
+
+    // 頂点uとvを繋げる関数(sizが大きい方に根を統合)
+    // 返り値はsameと同じ
+    bool unite(const int& u, const int& v) {
+        int rootU = root(u);
+        int rootV = root(v);
+        if (rootU == rootV) return 1;
+        if (siz[rootU] < siz[rootV]) {
+            par[rootU] = rootV;
+            siz[rootV] += siz[rootU];
+        } else {
+            par[rootV] = rootU;
+            siz[rootU] += siz[rootV];
+        }
+        return 0;
+    }
+};
+
+signed main(){
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+
+    int n,q;
+    cin >> n >> q;
+    int ans=10*(n-1);
+    v<UnionFind> uf(10,UnionFind(n));
+    rep(i,n-1+q) {
+        int a,b,c;
+        cin >> a >> b >> c;
+        --a,--b;
+        for (int j=c; j<10; ++j) {
+            if (!uf[j].unite(a,b)) --ans;
+        }
+        if (i>=n-1) print(ans);
+    }
+
+    return 0;
+}
