665afd69b93c04cf960a2429a2f70ed2e7b4d3fd
81-28 <eskrmc@gmail.com>
2026-01-13 00:20:37 +0900

    26_01_13

---
+++ archive/26_01_13/abc285_a.cpp
@@ -0,0 +1,14 @@
+// https://atcoder.jp/contests/abc285/tasks/abc285_a
+
+#include<bits/stdc++.h>
+using namespace std;
+
+signed main(){
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+
+    int a,b;
+    cin >> a >> b;
+    puts(a==b/2?"Yes":"No");
+
+    return 0;
+}
