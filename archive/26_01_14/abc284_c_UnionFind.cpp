b7c76759245da16ee0b3d39af9e2ff4e32a61a02
81-28 <eskrmc@gmail.com>
2026-01-14 10:33:27 +0900

    26_01_14

---
+++ archive/26_01_14/abc284_c_UnionFind.cpp
@@ -0,0 +1,58 @@
+// https://atcoder.jp/contests/abc284/tasks/abc284_c
+
+#include<bits/stdc++.h>
+using namespace std;
+
+template<typename Head,typename... Tail>
+void print(const Head &head,const Tail &... tail){cout<<head;((cout<<' '<<tail),...);cout<<endl;}
+
+
+class UnionFind {
+public:
+    // par: 親, siz: サイズ
+    vector<int> par,siz;
+    // n頂点のUnion-Find
+    UnionFind(const int& n) {
+        par = vector<int>(n,-1);
+        siz = vector<int>(n,1);
+    }
+
+    // 頂点xの根を返す関数
+    int root(const int& x) {
+        if (par[x] == -1) return x; // 親が無いとき、それが根
+        else return par[x] = root(par[x]); // 経路圧縮して親に移動
+    }
+
+    // 頂点uとvを繋げる関数(sizが大きい方に根を統合)
+    // 返り値はsameと同じ
+    bool unite(const int& u, const int& v) {
+        int rootU = root(u);
+        int rootV = root(v);
+        if (rootU == rootV) return 1;
+        if (siz[rootU] < siz[rootV]) {
+            par[rootU] = rootV;
+            siz[rootV] += siz[rootU];
+        } else {
+            par[rootV] = rootU;
+            siz[rootU] += siz[rootV];
+        }
+        return 0;
+    }
+};
+
+signed main(){
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+
+    int n,m;
+    cin >> n >> m;
+    UnionFind uf(n);
+    int ans=n;
+    while (m--) {
+        int u,v;
+        cin >> u >> v;
+        if (!uf.unite(--u,--v)) --ans;
+    }
+    print(ans);
+
+    return 0;
+}
