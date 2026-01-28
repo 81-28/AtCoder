90ffbe3e98f2543e9faddedb7b867e2e79cf120a
81-28 <eskrmc@gmail.com>
2026-01-28 23:05:51 +0900

    26_01_28

---
+++ archive/26_01_28/abc282_a.cpp
@@ -0,0 +1,17 @@
+// https://atcoder.jp/contests/abc282/tasks/abc282_a
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define rep(i,n) for(int i=0;i<(int)(n);++i)
+
+signed main(){
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+
+    int k;
+    cin >> k;
+    rep(i,k) cout<<char('A'+i);
+    cout<<endl;
+
+    return 0;
+}
