ef61c4aa310b617e698161d73ad895b0a38dbe76
8128 <eskrmc@gmail.com>
2026-02-16 23:22:21 +0900

    26_02_16

---
+++ archive/26_02_16/abc281_a.cpp
@@ -0,0 +1,14 @@
+// https://atcoder.jp/contests/abc281/tasks/abc281_a
+
+#include<bits/stdc++.h>
+using namespace std;
+
+signed main(){
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+
+    int n;
+    cin >> n;
+    for (int i=n; i>=0; --i) cout<<i<<endl;
+
+    return 0;
+}
