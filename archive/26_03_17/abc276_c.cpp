18d8bb47dbc983d71d94bfc932c3e5048dde4555
8128 <eskrmc@gmail.com>
2026-03-17 22:05:45 +0900

    26_03_17

---
+++ archive/26_03_17/abc276_c.cpp
@@ -0,0 +1,30 @@
+// https://atcoder.jp/contests/abc276/tasks/abc276_c
+
+#include<bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+using v=vector<T>;
+using vi=v<int>;
+#define all(v) v.begin(),v.end()
+
+template<typename T>
+istream &operator>>(istream &is,v<T> &v){for(T &in:v)is>>in;return is;}
+template<typename T>
+ostream &operator<<(ostream &os,const v<T> &v){for(int i=0;i<(int)v.size();++i)os<<(i?" ":"")<<v[i];return os;}
+template<typename Head,typename... Tail>
+void print(const Head &head,const Tail &... tail){cout<<head;((cout<<' '<<tail),...);cout<<endl;}
+
+
+signed main(){
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vi a(n);
+    cin >> a;
+    prev_permutation(all(a));
+    print(a);
+
+    return 0;
+}
