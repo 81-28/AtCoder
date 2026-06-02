1ff6bf873c54624a4e866bcd2db281004f6a7f37
8128 <eskrmc@gmail.com>
2026-06-02 13:55:07 +0900

    26_06_02

---
+++ archive/26_06_02/abc229_e_UnionFind.cpp
@@ -0,0 +1,73 @@
+// https://atcoder.jp/contests/abc229/tasks/abc229_e
+
+#include<bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+using v=vector<T>;
+using vi=v<int>;
+using vvi=v<vi>;
+#define rep1(i,n) for(int i=1;i<=(int)(n);++i)
+#define pb push_back
+
+template<typename Head,typename... Tail>
+void print(const Head &head,const Tail &... tail){cout<<head;((cout<<' '<<tail),...);cout<<endl;}
+
+
+class UnionFind {
+public:
+    // par: 親, siz: サイズ
+    vector<int> par,siz;
+    // n頂点のUnion-Find
+    UnionFind(const int& n) {
+        par = vector<int>(n,-1);
+        siz = vector<int>(n,1);
+    }
+
+    // 頂点xの根を返す関数
+    int root(const int& x) {
+        if (par[x] == -1) return x; // 親が無いとき、それが根
+        else return par[x] = root(par[x]); // 経路圧縮して親に移動
+    }
+
+    // 頂点uとvを繋げる関数(sizが大きい方に根を統合)
+    // 返り値はsameと同じ
+    bool unite(const int& u, const int& v) {
+        int rootU = root(u);
+        int rootV = root(v);
+        if (rootU == rootV) return 1;
+        if (siz[rootU] < siz[rootV]) {
+            par[rootU] = rootV;
+            siz[rootV] += siz[rootU];
+        } else {
+            par[rootV] = rootU;
+            siz[rootU] += siz[rootV];
+        }
+        return 0;
+    }
+};
+
+signed main(){
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+
+    int n,m;
+    cin >> n >> m;
+    vvi g(n+1);
+    while (m--) {
+        int a,b;
+        cin >> a >> b;
+        g[a].pb(b);
+    }
+    UnionFind uf(n+1);
+    int res=n;
+    vi ans(n+1);
+    for (int i=n; i; --i) {
+        ans[i]=res-i;
+        for (int nxt:g[i]) {
+            if (!uf.unite(nxt,i)) --res;
+        }
+    }
+    rep1(i,n) print(ans[i]);
+
+    return 0;
+}
