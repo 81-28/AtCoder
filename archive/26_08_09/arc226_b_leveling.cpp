d0a98bfc74ba1a3eb52dd8fef34de3bc04fea0bb
8128 <eskrmc@gmail.com>
2026-08-10 16:46:56 +0900

    26_08_10

--- archive/26_08_09/arc226_b_leveling.cpp
+++ archive/26_08_09/arc226_b_leveling.cpp
@@ -4,38 +4,25 @@
 using namespace std;
 
 using ll=long long;
-#define int ll
-template<typename T>
-using v=vector<T>;
-using vi=v<int>;
-template<typename T>inline bool chmax(T& a,const T& b){if(a<b){a=b;return 1;}return 0;}
-
-template<typename T>
-istream &operator>>(istream &is,v<T> &v){for(T &in:v)is>>in;return is;}
-#define endl '\n' // flushしたい場合は無効化
-template<typename Head,typename... Tail>
-void print(const Head &head,const Tail &... tail){cout<<head;((cout<<' '<<tail),...);cout<<endl;}
-
 
 signed main(){
     ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
 
-    int t;
+    ll t;
     cin >> t;
     while (t--) {
-        int n,m;
+        ll n,m;
         cin >> n >> m;
-        vi a(m);
-        cin >> a;
-        int ans=0;
-        int cnt=0;
+        vector<ll> a(m);
+        for (ll&v:a) cin >> v;
+        ll ans=0,cnt=0;
         // 水が同じ高さになるようなイメージ
-        for (int i=m-1; i>=0; --i) {
+        for (ll i=m-1; i>=0; --i) {
             cnt=cnt*2+a[i];
-            int need=(cnt+n-1)/n;
-            chmax(ans,need*(1LL<<i));
+            ll need=(cnt+n-1)/n;
+            ans=max(ans,need*(1LL<<i));
         }
-        print(ans);
+        cout<<ans<<'\n';
     }
 
     return 0;
---
+++ archive/26_08_10/abc189_c.cpp
@@ -0,0 +1,39 @@
+// https://atcoder.jp/contests/abc189/tasks/abc189_c
+
+#include<bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+using v=vector<T>;
+using vi=v<int>;
+#define rep(i,n) for(int i=0;i<(int)(n);++i)
+template<typename T>inline bool chmax(T& a,const T& b){if(a<b){a=b;return 1;}return 0;}
+template<typename T>inline bool chmin(T& a,const T& b){if(a>b){a=b;return 1;}return 0;}
+
+template<typename T>
+istream &operator>>(istream &is,v<T> &v){for(T &in:v)is>>in;return is;}
+#define endl '\n' // flushしたい場合は無効化
+template<typename Head,typename... Tail>
+void print(const Head &head,const Tail &... tail){cout<<head;((cout<<' '<<tail),...);cout<<endl;}
+
+
+signed main(){
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+    cout<<fixed<<setprecision(16);
+
+    int n;
+    cin >> n;
+    vi a(n);
+    cin >> a;
+    int ans=0;
+    rep(l,n) {
+        int mi=a[l];
+        for(int r=l+1; r<=n; ++r) {
+            chmax(ans,mi*(r-l));
+            chmin(mi,a[r]);
+        }
+    }
+    print(ans);
+
+    return 0;
+}
